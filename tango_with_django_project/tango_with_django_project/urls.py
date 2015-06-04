0346e88cfe5dcdaa35d339ff03f77117f9b72686
root <root@server.hcl.com>
2015-06-04 16:11:58 -0500

    04.06.2015

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import include, patterns, url
 from rango import views
+from django.contrib import admin
 
-urlpatterns = patterns('', url(r'^rango/', include('rango.urls')),)
+urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/', include('rango.urls')),)
