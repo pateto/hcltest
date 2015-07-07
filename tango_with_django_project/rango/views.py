e4042fbfa4f45ac82c7893e0a24f6133a3ec1ef0
root <root@server.hcl.com>
2015-07-07 17:42:36 -0500

    07.07.15

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -56,36 +56,44 @@ def about(request):
 	return render(request, 'rango/about.html', {'visits': count})
 
 def category(request, category_name_slug):
-    
-    #Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-    
-    try:
+	
+	#Create a context dictionary which we can pass to the template rendering engine.
+	context_dict = {}
+	
+	try:
         # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-        context_dict['category_name_slug'] = category.slug
-
-        # Retrieve all of the associated pages.
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name
+		context_dict['category_name_slug'] = category.slug
+		
+		# Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
+		pages = Page.objects.filter(category=category)
 
         # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
+		context_dict['pages'] = pages
 		
-    except Category.DoesNotExist:
+		# We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.		
+		context_dict['category'] = category
+		
+	except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything - the template displays the "no category" message for us.
-        pass
-        
-    # Go render the response and return it to the client.
-    return render(request, 'rango/category.html', context_dict)
+		pass
+		
+	result_list = []
+	if request.method == 'POST':
+		query = request.POST['query'].strip()
+		if query:
+			# Run our Bing function to get the results list!
+			result_list = run_query(query)
+			context_dict['result_list'] = result_list
+				
+	# Go render the response and return it to the client.
+	return render(request, 'rango/category.html', context_dict)
 
 @login_required
 def add_category(request):
@@ -139,15 +147,6 @@ def add_page(request, category_name_slug):
 @login_required
 def restricted(request):
 	return render(request, 'rango/restricted.html', {})
-
-def search(request):
-	result_list = []
-	if request.method == 'POST':
-		query = request.POST['query'].strip()
-		if query:
-			# Run our Bing function to get the results list!
-			result_list = run_query(query)
-	return render(request, 'rango/search.html', {'result_list': result_list})
 
 def track_url(request):	
 	if request.method == 'GET':
