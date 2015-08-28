6db339745b93ad5f34617fcf3a38fe469cfb4ca4
root <root@server.hcl.com>
2015-08-28 12:32:42 -0500

    28.08.2015

--- hcl/conta/urls.py
+++ hcl/conta/urls.py
@@ -4,6 +4,7 @@ from conta import views
 urlpatterns = patterns(
 		'',
         url(r'^$', views.index, name='index'),
-		url(r'^hoja_de_vida/1$', views.hoja_de_vida, name='hoja_de_vida'),
-		url(r'^hoja_de_vida/$', views.hoja_de_vida_pag, name='hoja_de_vida_pag'),
+		# ex: /nhva01/1/
+		url(r'^nhva01/(?P<nhva01_id>[0-9]+)/$', views.nhva01_detail, name='nhva01_detail'),
+		url(r'^nhva01/$', views.nhva01, name='nhva01'),
 	)
--- hcl/conta/views.py
+++ hcl/conta/views.py
@@ -2,33 +2,36 @@ from django.shortcuts import render, render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from conta.models import NHva01
 from .forms import NHva01Form
-#import pdb
+import pdb
 #pdb.set_trace()
 
 def index(request):		
 	response = render(request, 'conta/index.html', {})	
 	return response
 
-def hoja_de_vida(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NHva01Form(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
+def nhva01_detail(request, nhva01_id):
+	
+	#pdb.set_trace()
+	# if this is a POST request we need to process the form data
+	if request.method == 'POST':
+		# create a form instance and populate it with data from the request:
+		form = NHva01Form(request.POST)
+		# check whether it's valid:
+		if form.is_valid():
+			# process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+			print 'is valid'
+			
+	# if a GET (or any other method) we'll create a blank form
+	else:
+		nhva01 = NHva01.objects.get(pk=nhva01_id)
+		form = NHva01Form(instance=nhva01)
+		
+	return render(request, 'conta/nhva01.html', {'form': form})
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NHva01Form()
-
-    return render(request, 'conta/hoja_de_vida.html', {'form': form})
-
-def hoja_de_vida_pag(request):	
-	#nhva01_list = NHva01.objects.all()[:100]
+def nhva01(request):	
+	
 	nhva01_list = NHva01.objects.all()
 	paginator = Paginator(nhva01_list, 25)
 	
