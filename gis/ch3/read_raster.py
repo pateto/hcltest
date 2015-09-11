6c23005899486093a6cc4e720065afb66ab407db
root <root@server.hcl.com>
2015-09-11 17:56:25 -0500

    11.09.2015

---
+++ gis/ch3/read_raster.py
@@ -0,0 +1,17 @@
+from osgeo import gdal, gdalconst
+import struct
+import pdb
+
+pdb.set_trace()
+dataset = gdal.Open("1.tif")
+band = dataset.GetRasterBand(1)
+
+fmt = "<" + ("h" * band.XSize)
+totHeight = 0
+for y in range(band.YSize):
+	scanline = band.ReadRaster(0, y, band.XSize, 1, band.XSize, 1, band.DataType)
+	values = struct.unpack(fmt, scanline)
+	for value in values:
+		totHeight = totHeight + value
+	average = totHeight / (band.XSize * band.YSize)
+print "Average height =", average
